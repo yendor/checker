d4885c5531fc1cf85ae55f67130068b886dacb5e
Rodney Amato <rodnet@gmail.com>
2012-12-18 10:48:13 +1100

    Pad out the basic checker logic

--- checker.go
+++ checker.go
@@ -4,40 +4,52 @@ package main
 
 import "fmt"
 import "io/ioutil"
-//import "os"
+import "os"
 import "flag"
+import "crypto/md5"
+//import "io"
+//import "bytes"
 
 func main() {
     flag.Parse()
 
     // TODO: Make sure the supplied argument is a directory
-
-    checksums := FileChecksums(flag.Arg(0))
-    for k, v := range checksums {
-        fmt.Printf( "The checksum of %s is %s\n", k, v)
-    }
+    FileChecksums(flag.Arg(0))
 }
 
-func FileChecksums(dir string) map[string]string {
-    files, err := ioutil.ReadDir(dir)
+func FileChecksums(dir string) {
+    _, err := os.Stat(dir)
     if err != nil {
         panic(err)
     }
-    hashes := make(map[string]string)
-    hash := "abcdef"
 
+    files, err := ioutil.ReadDir(dir)
+
+    if err != nil {
+        panic(err)
+    }
+
+    hash := "abcdef"
 
     for i := 0; i < len(files); i++ {
         fullpath := dir + "/" + files[i].Name()
+
         if files[i].IsDir() {
-            for k, v := range FileChecksums(fullpath) {
-                hashes[k] = v
-            }
+            FileChecksums(fullpath);
         } else {
             // TODO: Calculate the hash of the file
-            hashes[fullpath] = hash
+            hash = FileChecksum(fullpath)
+            fmt.Printf( "%s  %s\n", hash, fullpath)
         }
     }
+}
 
-    return hashes
+func FileChecksum(filepath string) string {
+    contents, err := ioutil.ReadFile(filepath)
+    if err != nil {
+        panic(err)
+    }
+    running_hash := md5.New()
+    running_hash.Write(contents)
+    return fmt.Sprintf("%x", running_hash.Sum(nil))
 }
